8f59d7b5a1c012965b37c19b9a2f121a17b67e8e
Hady Alkadri <98332279+hadyalkadri@users.noreply.github.com>
2022-11-28 19:28:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ const port = process.env.PORT || 3002;
 const app = express();
 app.use(cors());
 
-app.use(express.static(path.join(__dirname + "/public")))
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, '/public', 'index.html'))
-})
+//app.use(express.static(path.join(__dirname + "/public")))
+//app.get("*", (req, res) => {
+//    res.sendFile(path.join(__dirname, '/public', 'index.html'))
+//})
 
 // for parsing form data in format x--www....
 app.use(bodyParser.urlencoded({ extended: false }));
